85e5fe4175b7e5bf6323f1492e87cc15975a25d5
Ivan <smow32da@gmail.com>
2018-11-17 23:39:44 +0500

    Исправил недочеты

--- lesson1/script.js
+++ lesson1/script.js
@@ -4,22 +4,22 @@ let money = +prompt("Ваш бюджет за месяц?", "");
 let time = prompt("Введите дату в формате YYYY-MM-DD", "2018-11-15");
 
 let mandatoryArticle1 = prompt("Введите обязательную статью расходов в этом месяце", "");
-let cost1 = +prompt("Во сколько обойдется?", "");
+let secondAnswer = +prompt("Во сколько обойдется?", "");
 let mandatoryArticle2 = prompt("Введите обязательную статью расходов в этом месяце", "");
 let cost2 = +prompt("Во сколько обойдется?", "");
+
 
 let appData = {
     budget: money,
     timeData: time,
     expenses: {
-        mandatoryArticle: cost1,
-        mandatoryArticle2: cost2
     },
     optionalExpenses: {},
     income: [],
     savings: false
 };
+appData.expenses[mandatoryArticle1] = secondAnswer;
+appData.expenses[mandatoryArticle2] = cost2;
 
-let moneyDay = (money - (cost1 + cost2)) / 30;
+let moneyDay = (money - (cost2 + secondAnswer)) / 30;
 alert("Ваш бюджет на один день составит: " + moneyDay + " руб.");
-
