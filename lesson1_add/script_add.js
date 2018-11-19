6ff8f4b2197b1d1287708dc773d26541cfa941b0
Ivan <smow32da@gmail.com>
2018-11-19 17:14:46 +0500

    Изменение 1

--- lesson1_add/script_add.js
+++ lesson1_add/script_add.js
@@ -3,6 +3,7 @@
 let num = 33721;
 let numArray = String(num).split("");
 let result = ((numArray[0] * numArray[1] * numArray[2] * numArray[3] * numArray[4]) ** 3);
+console.log(result);
 let resultArray = String(result).split("");
 alert(resultArray[0] + resultArray[1]);
 
