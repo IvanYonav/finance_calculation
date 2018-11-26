8fb3d7c51fe7addc3fd63d495ae8fab7acc6fbd0
Ivan <smow32da@gmail.com>
2018-11-26 21:17:24 +0500

    Сдаланно лекционное домашнее задание

--- lecture1/script.js
+++ lecture1/script.js
@@ -1,11 +1,17 @@
 
-
-
-
-let obj = {
-    changeLetters = cutWord.slice(0, 100);
-        alert(changeLetters);
+function selection(a, b) {
+    let arr = [];
+    outer: for (var i = a; i <= b; i++) {
+        for (var j = 2; j < i; j++) {
+            if (i % j == 0) continue outer;
+        }
+        arr.push(i);
+    }
+    arr.forEach(function (item, i) {
+    if (++i) {
+        document.write('<p>' + item + ' - делители этого числа: ' + 1 + ' и ' + item + '</p>');
+    }
+});
 };
-
-
+selection(2, 100);
 
